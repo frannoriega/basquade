6e33fcdd81028e6638d4f12269c39883e2f23d42
Francisco Noriega <fran@readyset.io>
2024-04-23 17:45:36 -0300

    ui: Added dummy categories to test layout

--- components/layout/aside.tsx
+++ components/layout/aside.tsx
@@ -1,6 +1,3 @@
-import { getDocuments } from "@/data/documents";
-import DocumentPreview from "../document-preview";
-import Link from "next/link";
 import { getCategories } from "@/data/categories";
 type Props = {
   className?: string;
@@ -12,11 +9,18 @@ export default async function Aside({ className }: Props) {
   if (!categories) {
     return null;
   }
+  let len = categories.length;
 
   return (
     <div className="overflow-auto">
       <div className="flex h-max gap-2 overflow-auto bg-blue-300 py-2 md:flex-col">
-        {categories.map(({ id, name }, number) => (
+        {categories.concat(Array.from({ length: 30 }, (_, i) => (
+          {
+            id: len + i,
+            name: `Dummy ${i}`,
+            slug: i.toString(),
+          }
+        ))).map(({ id, name }, number) => (
           <div key={id} className="flex-grow">
             {/* <Link key={number} href={`/categories/${id}`} passHref> */}
             <span>{name}</span>
