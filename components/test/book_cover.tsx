da665f049c3820d4d5143aad699e050ef878e26a
Francisco Noriega <fran@readyset.io>
2024-05-06 00:04:42 -0300

    lint: Fixed lint warning regarding img tags
    We were using `<img>` tags, but Next.js suggests we use `<Image>`
    instead, so I did just that.

--- components/test/book_cover.tsx
+++ components/test/book_cover.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image'
+
 export default function BookPreview({ id }: { id: number; }) {
   const bookId = (id % 3) + 1;
   return (
     // TODO: Is it ok to "hardcode" height here?
-    <img className="md:h-72 h-56 overflow-clip shadow-md shadow-black" src={`/images/cover-samples/b${bookId}.jpg`}></img>
+    <Image height={224} width={500} alt="libro" src={`/images/cover-samples/b${bookId}.jpg`}></Image>
   )
 }
