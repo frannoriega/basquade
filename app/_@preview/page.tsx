54d5a21a1f99923de2a0860cf125b1e11b84e4f0
Francisco Noriega <frannoriega.92@gmail.com>
2024-07-07 15:47:47 -0300

    fix: Removed @preview page

--- app/_@preview/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    // TODO: Auto-height
-    <div className="h-[2000px] border-4 border-dotted border-red-800 bg-white">
-      <h1>Base page</h1>
-    </div>
-  );
-}
