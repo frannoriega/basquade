121536063a68a4ce08647885bae76f12216995b6
Francisco Noriega <frannoriega.92@gmail.com>
2024-07-10 10:10:18 -0300

    footer: Puse color para distinguir el footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,7 @@ import NextIcon from "@/assets/next.svg";
 
 export default function Footer() {
   return (
-    <div className="h-16 flex flex-row">
+    <div className="bg-blue-400 h-16 flex flex-row">
       {/* TODO: Facebook */}
       <NextIcon />
       {/* TODO: Instagram */}
