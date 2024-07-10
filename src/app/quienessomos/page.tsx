64422f5a80ee70070fff772103acab3314139cbb
Francisco Noriega <frannoriega.92@gmail.com>
2024-07-10 10:13:49 -0300

    quienessomos: Añadí la sección "Quienes Somos"
    Por ahora, esta página es simplemente un `lorem ipsum`. Después hay que
    rellenarla con el contenido real.

---
+++ src/app/quienessomos/page.tsx
@@ -0,0 +1,18 @@
+
+export default function QuienesSomos() {
+  return (
+    <div className="">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In odio nibh, laoreet et velit at, aliquam ornare risus. Sed in dolor ac mauris placerat placerat vel sed dui. Suspendisse a orci sed tellus auctor condimentum in eget urna. Proin accumsan nisl mauris, ullamcorper aliquet ex ultricies eget. Quisque dui eros, rhoncus non orci in, faucibus malesuada eros. Cras et erat lacinia tortor tempor scelerisque. Aenean feugiat sed libero a molestie.
+
+        Proin tristique nisl pellentesque euismod tincidunt. Nullam molestie est et condimentum lacinia. Nunc gravida, diam dictum malesuada semper, risus lectus sagittis lectus, non rhoncus mauris odio quis libero. Donec ac velit ut elit consequat tincidunt tristique quis sem. Quisque a euismod augue. Morbi vehicula est ut sollicitudin dapibus. Cras a est sit amet sapien mattis mollis id eget justo. Aliquam vulputate est eget risus gravida, vel efficitur erat blandit. Phasellus nibh nisl, laoreet et mauris id, sodales fringilla tellus. Phasellus orci felis, sagittis vitae elit bibendum, porttitor imperdiet odio. Fusce dictum fermentum orci, vel mattis tellus aliquet vitae. Maecenas accumsan, elit vel maximus congue, magna felis laoreet augue, ac vulputate metus neque quis eros.
+
+        Praesent dignissim magna eu nulla vehicula molestie. Proin id magna magna. Nunc ultricies leo diam, in ornare magna ultricies sit amet. Curabitur vitae euismod turpis. Nam rutrum dapibus sapien sit amet rhoncus. Morbi a tortor in felis viverra laoreet eu eu ex. Donec ipsum dolor, vehicula vitae sollicitudin eget, facilisis nec eros. Sed porta gravida lobortis.
+
+        Ut sapien lorem, pulvinar sit amet urna vitae, porta bibendum nisi. Proin pulvinar est eu tempus rhoncus. Suspendisse potenti. Nam et orci sit amet nibh dignissim feugiat. Donec sagittis dignissim sem in aliquam. Aliquam et tellus vitae justo facilisis vulputate id et ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse odio ligula, ultrices nec orci ac, feugiat facilisis nisi. Integer id mauris consectetur, molestie felis non, gravida sem. Donec sed nisl lorem. Ut sem elit, iaculis eu gravida id, auctor id dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra eu nibh non fringilla. Proin nibh turpis, dapibus nec blandit a, finibus ac ante. Curabitur condimentum nunc quis nunc fringilla, quis viverra est efficitur.
+
+        Aenean ac mattis nunc. Nam nisl enim, fermentum sed aliquet vitae, ultrices luctus nulla. Integer sed porta sem. Fusce elementum sed est ac egestas. Donec diam sapien, rutrum vehicula eros non, rhoncus fermentum nisl. Mauris nec pharetra mauris, a dapibus orci. Suspendisse condimentum rhoncus turpis, vel iaculis odio. Duis placerat massa massa, nec finibus leo ultricies iaculis.
+      </p>
+    </div>
+  )
+}
