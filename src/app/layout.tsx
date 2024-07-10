a71543d3b3b377345f2e472490a3d52dcadfa4fe
Francisco Noriega <frannoriega.92@gmail.com>
2024-07-09 23:09:32 -0300

    body: Simplifique las clases del body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
 <link crossOrigin="use-credentials" href="https://fonts.gstatic.com" rel="preconnect" />
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"/>
       </head>
-      <body className={`bg-orange-300 flex flex-col w-full justify-stretch min-h-screen font-roboto ${inter.className}`}>
+      <body className={`bg-orange-300 w-full font-roboto ${inter.className}`}>
         <Navbar />
           <main className="flex-1 overflow-y-auto w-full bg-indigo-100">{children}</main>
       </body>
