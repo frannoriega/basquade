259883e640955005f59cee34d0a89dd7aab65dd2
Francisco Noriega <frannoriega.92@gmail.com>
2024-07-29 17:45:48 -0300

    sql: Agregué documentación sobre las tablas
    Agregué un par de comentarios a los archivos sql, para que quede más claro que propósito tiene cada
    tabla y sus campos.

--- docker/sql/001 - languages.sql
+++ docker/sql/001 - languages.sql
@@ -1,4 +1,7 @@
+-- Los lenguajes presentes en los pdfs
 CREATE TABLE lang (
+  -- Un identificador autogenerado
   id SERIAL NOT NULL PRIMARY KEY,
+  -- El lenguaje en si
   language VARCHAR(16) NOT NULL UNIQUE
 );
--- docker/sql/002 - books.sql
+++ docker/sql/002 - books.sql
@@ -1,10 +1,20 @@
+-- Tabla para guardar los pdfs
 CREATE TABLE books (
+  -- Un identificador autogenerado
   id SERIAL NOT NULL PRIMARY KEY,
+  -- El título del pdf
   title VARCHAR(255) NOT NULL,
+  -- El pdf en sí mismo
   file BYTEA NOT NULL,
+  -- El texto de pdf, convertido en un TS VECTOR
+  -- para poder realizar busquedas sobre el texto
   content TSVECTOR NOT NULL,
+  -- El idioma del pdf
   lang INT NOT NULL REFERENCES lang(id)
+  -- El hash MD5 del pdf
   md5 TEXT NOT NULL UNIQUE
 );
 
+-- Un indice para realizar busquedas rapidas sobre
+-- el texto de los pdfs
 CREATE INDEX books_content_idx ON books USING gin (content);
