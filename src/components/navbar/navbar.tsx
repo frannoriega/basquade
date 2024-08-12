028a572c96de0cb63d22aeaf084b0908ef3906e7
Francisco Noriega <frannoriega.92@gmail.com>
2024-08-12 18:52:08 -0300

    navbar: Agregué palabra "Biblioteca" al "logo"
    Estuve probando distintas formas de que la página resalte. Me gustó cómo
    se vió la palabra "Biblioteca", y la dejé.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,7 +4,9 @@ import SearchBar from "@/components/navbar/search/search";
 export default function Navbar() {
   return (
     <div className="flex h-16 min-w-fit flex-row items-center justify-between gap-4 bg-slate-50 pl-4 pr-4">
-      <a href="/"><h1 className="text-3xl">Basqüadé</h1></a>
+      <a href="/">
+        <h1 className="flex flex-col items-center">Biblioteca <span className="text-3xl font-black">Basqüadé</span></h1>
+      </a>
       <div className="flex flex-row items-center md:gap-8 gap-6">
         <SearchBar />
         <Actions />
