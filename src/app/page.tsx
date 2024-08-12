5229f447e4c612dd479cf8519c5638566432a011
Francisco Noriega <frannoriega.92@gmail.com>
2024-08-12 18:53:05 -0300

    sections: Hice que los botones de sección tengan fondo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
           categories.map((category, _) =>
             // TODO: Investigar cómo hacer para que el tamaño sea parejo sin definir altura fija
             // (h-?)
-            <li className="grid grid-rows-2 justify-self-center w-52 h-48 items-center p-4 gap-4 border-2 border-slate-800 rounded-md hover:shadow-lg hover:border-green-600 hover:text-green-600 hover:fill-green-600" key={category.name}>
+            <li className="bg-slate-50 grid grid-rows-2 justify-self-center w-52 h-48 items-center p-4 gap-4 border-2 border-slate-800 rounded-md hover:shadow-lg hover:border-green-600 hover:text-green-600 hover:fill-green-600" key={category.name}>
                 {category.icon}
               <span className="text-center block align-middle">{category.name}</span>
             </li>)
