51506f3138ed952ac3a5fcab78e2efc8a5e17252
Francisco Noriega <frannoriega.92@gmail.com>
2024-08-12 18:51:38 -0300

    footer: Agregué iconos de redes sociales al footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,14 +1,25 @@
-import NextIcon from "@/assets/next.svg";
+import Facebook from "@/assets/icons/social/facebook.svg";
+import Twitter from "@/assets/icons/social/twitter.svg";
+import Instagram from "@/assets/icons/social/instagram.svg";
 
 export default function Footer() {
   return (
-    <div className="bg-blue-400 h-16 flex flex-row">
-      {/* TODO: Facebook */}
-      <NextIcon />
-      {/* TODO: Instagram */}
-      <NextIcon />
-      {/* TODO: Twitter? */}
-      <NextIcon />
-    </div>
+    <ul className="pl-4 bg-green-200 h-12 flex flex-row items-center gap-4">
+      <li>
+        <a href='#'>
+          <Facebook className="h-6"/>
+        </a>
+      </li>
+      <li>
+        <a href='#'>
+          <Twitter className="h-6"/>
+        </a>
+      </li>
+      <li>
+        <a href='#'>
+          <Instagram className="h-6"/>
+        </a>
+      </li>
+    </ul>
   )
 }
