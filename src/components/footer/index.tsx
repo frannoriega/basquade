84652fc8e388562bbb9fd585a08910f9a6a29da2
Francisco Noriega <frannoriega.92@gmail.com>
2024-09-18 11:10:13 -0300

    footer: Arreglé iconos del footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,23 +1,23 @@
-import Facebook from "@/assets/icons/social/facebook.svg";
-import Twitter from "@/assets/icons/social/twitter.svg";
-import Instagram from "@/assets/icons/social/instagram.svg";
+import FacebookIcon from "./facebook-icon";
+import InstagramIcon from "./instagram-icon";
+import TwitterIcon from "./twitter-icon";
 
 export default function Footer() {
   return (
     <ul className="pl-4 bg-green-200 dark:bg-slate-900 h-12 flex flex-row items-center gap-4">
       <li>
-        <a href='#'>
-          <Facebook className="h-6"/>
+        <a href='#' aria-label="Facebook">
+          <FacebookIcon className="h-6 dark:fill-slate-50 fill-slate-900"/>
         </a>
       </li>
       <li>
-        <a href='#'>
-          <Twitter className="h-6"/>
+        <a href='#' aria-label="Twitter">
+          <TwitterIcon className="h-6 dark:fill-slate-50 fill-slate-900"/>
         </a>
       </li>
       <li>
-        <a href='#'>
-          <Instagram className="h-6"/>
+        <a href='#' aria-label="Instagram">
+          <InstagramIcon className="h-6 dark:fill-slate-50 fill-slate-900"/>
         </a>
       </li>
     </ul>
