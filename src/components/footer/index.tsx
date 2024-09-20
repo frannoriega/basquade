3b134c92884640fed646808283eeed7a97cd0d57
Francisco Noriega <frannoriega.92@gmail.com>
2024-09-20 06:44:57 -0300

    style: Bordes para navbar y footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import TwitterIcon from "./twitter-icon";
 
 export default function Footer() {
   return (
-    <ul className="pl-4 bg-slate-50 dark:bg-slate-900 h-12 flex flex-row items-center gap-4">
+    <ul className="pl-4 bg-slate-50 dark:bg-slate-900 h-12 flex flex-row items-center gap-4 border-slate-200 border-t dark:border-slate-800">
       <li>
         <a href='#' aria-label="Facebook">
           <FacebookIcon className="h-6 dark:fill-slate-50 fill-slate-900"/>
