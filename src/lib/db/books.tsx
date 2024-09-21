f9e88a3ce57b7803475d7f8822345656f7bac25d
Francisco Noriega <frannoriega.92@gmail.com>
2024-09-21 12:09:40 -0300

    db: Cambios en el esquema de Prisma

--- src/lib/db/books.tsx
+++ src/lib/db/books.tsx
@@ -2,7 +2,9 @@ import prisma from "@/lib/prisma";
 import { Book } from "@prisma/client";
 import { exists } from "./categories";
 
-async function getBooks(cat: number): Promise<Book[] | null> {
+// Devuelve todos los libros no pendientes
+// asociados a una categoría, o `null` si la categoría no existe.
+async function getBooks(cat: number, page: number = 0, limit: number = 10): Promise<Book[] | null> {
   return exists(cat).then((e) => {
     if (!e) {
       return new Promise((resolve) => resolve(null))
@@ -10,29 +12,60 @@ async function getBooks(cat: number): Promise<Book[] | null> {
       return prisma.book.findMany({
         where: {
           categoryId: cat,
-        }
+          pending: false,
+        },
+        orderBy: {
+          title: "asc"
+        },
+        skip: Math.abs(page-1) * limit,
+        take: limit
       })
     }
   })
 }
 
-async function searchBooks(term: string): Promise<Book[]> {
-  return prisma.$queryRaw`SELECT b.id, title FROM "Book" as b JOIN "Lang" as l on b."langId" = l.id where b.content @@ websearch_to_tsquery(CAST(l.language AS regconfig), '${term}')`
+// Busca los libros basados en la frase (`term`) pasada
+// como parámetro
+async function searchBooks(term: string, page: number = 0, limit: number = 10): Promise<Book[]> {
+  return prisma.$queryRaw`SELECT b.id, title FROM "Book" as b JOIN "Lang" as l on b."langId" = l.id where b.content @@ websearch_to_tsquery(CAST(l.language AS regconfig), '${term}') WHERE b.pending = false ORDER BY b.title ASC LIMIT ${limit} OFFSET ${Math.abs(page-1) * limit}`
 }
 
-async function searchBooksFromCategory(term: string, filter: string): Promise<Book[]> {
-  return prisma.$queryRaw`SELECT b.id, title  FROM "Book" as b JOIN "Lang" as l on b."langId" = l.id where b.content @@ websearch_to_tsquery(CAST(l.language AS regconfig), '${term}') AND b."categoryId" = ${filter}`
+// Busca los libros en una categoría específica, basado
+// en la frase (`term`) pasada como parámetro
+async function searchBooksFromCategory(
+  term: string,
+  filter: string,
+  page: number = 0,
+  limit: number = 0
+): Promise<Book[]> {
+  return prisma.$queryRaw`SELECT b.id, title  FROM "Book" as b JOIN "Lang" as l on b."langId" = l.id where b.content @@ websearch_to_tsquery(CAST(l.language AS regconfig), '${term}') AND b."categoryId" = ${filter} WHERE b.pending = false ORDER BY b.title ASC LIMIT ${limit} OFFSET ${Math.abs(page-1) * limit}`
 }
 
-async function getPDF(id: number): Promise<Buffer> {
-  return prisma.book.findUniqueOrThrow({
+// Devuelve el PDF asociado al libro, o `null` en caso
+// de que no exista
+async function getPDF(id: number): Promise<Buffer | null> {
+  return prisma.book.findUnique({
     select: {
       file: true
     },
     where: {
-      id: id
+      id: id,
+      pending: false
     }
-  }).then((book) => book.file)
+  }).then((book) => book ? book.file : null)
+}
+
+// Devuelve algún PDF que esté pendiente de corrección,
+// o `null` si no hay ninguno pendiente
+async function getPendingPDF(): Promise<Buffer | null> {
+  return prisma.book.findFirst({
+    select: {
+      file: true
+    },
+    where: {
+      pending: true
+    }
+  }).then((book) => book ? book.file : null)
 }
 
 export { getBooks, searchBooks, searchBooksFromCategory, getPDF };
