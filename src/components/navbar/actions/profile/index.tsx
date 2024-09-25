d2c461079a4c158ff201d116700034c3e24443df
Francisco Noriega <frannoriega.92@gmail.com>
2024-09-25 15:43:25 -0300

    style: Cambios en el estilo del perfil

--- src/components/navbar/actions/profile/index.tsx
+++ src/components/navbar/actions/profile/index.tsx
@@ -1,30 +1,36 @@
 'use client'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Profile() {
   const router = useRouter();
   const session = useSession();
-  const sessionItem = session?.data?.user ?
-    (
-      <>
-        <DropdownMenuItem className="dark:hover:bg-slate-700" onClick={() => router.push('/pending')}>Pendientes</DropdownMenuItem>
-        <DropdownMenuItem className="dark:hover:bg-slate-700" onClick={() => signOut()}>Cerrar sesión</DropdownMenuItem>
-      </>
-    ) :
-    <DropdownMenuItem onClick={() => signIn('google', { callbackUrl: '/admin' })}>Iniciar sesión</DropdownMenuItem>
+
+  if (session?.status == 'loading') {
+    return <Avatar>
+      <AvatarFallback className="animate-pulse"></AvatarFallback>
+    </Avatar>
+  }
+  if (!session?.data?.user) {
+    return <Link href="/auth/signin" className="hover:underline hover:underline-offset-8">Administrar</Link>
+  }
+
+  const avatar = session?.data?.user?.image || undefined;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarImage referrerPolicy='no-referrer' src={avatar} alt='Imagen de usuario'/>
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="p-2 w-48 dark:bg-slate-800" align="end">
-        {sessionItem}
+        <DropdownMenuItem className="dark:hover:bg-slate-700" onClick={() => router.push('/pending')}>Pendientes</DropdownMenuItem>
+        <DropdownMenuItem className="dark:hover:bg-slate-700" onClick={() => signOut()}>Cerrar sesión</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
