31f912c0b7b073749462e7bdfc779eb48cfabaa3
Francisco Noriega <frannoriega.92@gmail.com>
2024-09-30 14:18:26 -0300

    gallery: Mejoré la visualización de los libros
    Ahora los libros se muestran en tarjetas con titlo + portada, y tienen
    un hover que muestra la descripción

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,8 +1,13 @@
 import { BookPreview } from "@/data/book-preview";
 import Link from "next/link";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import Image from "next/image"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 type GalleryParams = {
   books: BookPreview[]
@@ -13,19 +18,31 @@ export default function Gallery({ books, className, ...props }: GalleryParams) {
   return (
     <ol className={cns} {...props}>
       {
-        books.map((b) =>
-        {
-            const base64String = b.cover.toString("base64")
-            return <Link key={b.id} href={`/api/pdf/${b.id}`}>
-              <li>
-                <div className="flex flex-col border border-slate-300 rounded-sm overflow-hidden">
-                  <Image className="h-96" src={`data:image/png;base64,${base64String}`} alt={b.title} width={256} height={362}></Image>
-                  <span>{b.title}</span>
-                  <span>{b.description}</span>
-                </div>
-              </li>
-            </Link>
-          }
+        books.map((b) => {
+          const base64String = b.cover.toString("base64")
+          return (
+            <HoverCard>
+              <HoverCardTrigger asChild>
+                <Link key={b.id} href={`/api/pdf/${b.id}`} className="flex flex-col w-fit">
+                  <li className="w-full">
+                    <Card>
+                      <CardTitle className="text-center p-2">{b.title}</CardTitle>
+                      <VisuallyHidden>
+                        <CardDescription>{b.description}</CardDescription>
+                      </VisuallyHidden>
+                      <CardContent className="flex flex-col">
+                        <img className="h-96 w-full" src={`data:image/png;base64,${base64String}`} alt={b.title} width={256} height={362}></img>
+                      </CardContent>
+                    </Card>
+                  </li>
+                </Link>
+              </HoverCardTrigger>
+              <HoverCardContent side="right">
+                {b.description}
+              </HoverCardContent>
+            </HoverCard>
+          )
+        }
         )
       }
     </ol>
