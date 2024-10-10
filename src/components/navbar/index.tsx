153d90205981e0e4397e04aa3d13cfbde61e4bfa
Francisco Noriega <frannoriega.92@gmail.com>
2024-10-10 09:40:19 -0300

    navbar: Mejore el titulo y el logo. Se simplificaron

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,17 +2,16 @@ import Actions from "@/components/navbar/actions";
 import SearchBar from "@/components/navbar/search";
 import { getShelves } from "@/lib/db/shelves";
 import { Sprout } from "lucide-react";
+import Link from "next/link";
 
 export default async function Navbar() {
   const shelves = await getShelves()
   return (
     <div className="sticky top-0 z-10 flex h-20 w-full flex-row items-center justify-between gap-4 dark:bg-gray-950/70 bg-gray-50/70 backdrop-blur-md pl-4 sm:pl-8 pr-8 border-gray-300/70 dark:border-gray-800 border-b">
-      <div className="flex flex-row items-center gap-4">
-        <Sprout className="h-12 w-12 sm:h-14 sm:w-14 self-end" />
-        <a href="/">
-          <h1 className="text-sm sm:text-base flex flex-col items-center">Biblioteca <span className="text-xl sm:text-3xl font-black">Basqüadé</span></h1>
-        </a>
-      </div>
+      <Link href="/" className="flex flex-row items-center gap-1">
+        <Sprout className="h-6 w-6 sm:h-8 sm:w-8" />
+        <h1 className="items-center text-xl sm:text-3xl font-black">Basqüadé</h1>
+      </Link>
       <div className="flex flex-row items-center md:gap-8 gap-6">
         <SearchBar shelves={shelves.map((c) => { return { id: c.id, name: c.name } })} />
         <Actions />
