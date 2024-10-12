cd31db72dcf5caa294e255e0e377124b8b54088a
Francisco Noriega <frannoriega.92@gmail.com>
2024-10-12 01:03:11 -0300

    admin: Lista la navegación en panel de admin para mobile

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,46 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+'use client'
+import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@radix-ui/react-navigation-menu";
 import { LayoutDashboard, Settings, UsersRound, Map, ListTodo, BookText, Globe } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
+
+const links = [
+  {
+    title: 'Dashboard',
+    link: '/admin',
+    icon: <LayoutDashboard />
+  },
+  {
+    title: 'Administradores',
+    link: '/admin/administradores',
+    icon: <UsersRound />
+  },
+  {
+    title: 'Libros',
+    link: '/admin/libros',
+    icon: <BookText />
+  },
+  {
+    title: 'Pendientes',
+    link: '/admin/pendientes',
+    icon: <ListTodo />
+  },
+  {
+    title: 'Mapas conceptuales',
+    link: '/admin/mapas',
+    icon: <Map />
+  },
+  {
+    title: 'Atlas conceptual',
+    link: '/admin/atlas',
+    icon: <Globe />
+  }
+]
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  const [selected, setSelected] = useState(0)
   return (
     <>
       <div className="hidden lg:grid mx-8 grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 rounded-lg overflow-hidden w-full pt-12 pb-12">
@@ -13,49 +50,36 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
             <h1 className="text-xl self-center font-bold">Administración</h1>
           </div>
           <ul className="flex flex-col">
-            <PanelOption href="/admin">
-              <LayoutDashboard />
-              <h2>Dashboard</h2>
-            </PanelOption>
-            <PanelOption href="/admin/administradores">
-              <UsersRound />
-              <h2>
-                Administradores
-              </h2>
-            </PanelOption>
-            <PanelOption href="/admin/libros">
-              <BookText />
-              <h2>Libros</h2>
-            </PanelOption>
-            <PanelOption href="/admin/pendientes">
-              <ListTodo />
-              <h2>Pendientes</h2>
-            </PanelOption>
-            <PanelOption href="/admin/mapas">
-              <Map />
-              <h2>Mapas conceptuales</h2>
-            </PanelOption>
-            <PanelOption href="/admin/atlas">
-              <Globe />
-              <h2>Atlas conceptual</h2>
-            </PanelOption>
+            {links.map((e) => (
+              <PanelOption key={e.link} href={e.link}>
+                {e.icon}
+                <h2>{e.title}</h2>
+              </PanelOption>
+            ))}
           </ul>
         </div>
-        <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        <div className="lg:col-span-3 xl:col-span-4">
           {children}
         </div>
       </div>
-      <div className="block lg:hidden">
-        Se puede
-        <Select>
-          <SelectTrigger>
-            <SelectValue defaultValue="admin" />
-          </SelectTrigger>
-          <SelectContent className="">
-            <SelectItem value="dashboard">Dashboard</SelectItem>
-            <SelectItem value="admin">Administradores</SelectItem>
-          </SelectContent>
-        </Select>
+      <div className="flex flex-col w-full items-center justify-items-center pt-4 lg:hidden">
+        <NavigationMenu className="h-fit bg-accent rounded-xl overflow-hidden">
+          <NavigationMenuList className="flex flex-row">
+            {links.map((e, i) => (
+              <NavigationMenuItem key={e.link}>
+                {/* TODO: Implement pending page */}
+                <Link href={e.link} legacyBehavior passHref>
+                  <NavigationMenuLink onClick={() => setSelected(i)} className={cn(buttonVariants({ variant: 'default', size: 'sm'}), `px-3 py-6 rounded-none bg-inherit text-inherit hover:bg-gray-200 dark:hover:bg-gray-700 ${selected == i ? "bg-gray-200 dark:bg-gray-700" : null}`)}>
+                    {e.icon}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="w-full">
+          {children}
+        </div>
       </div>
     </>
   )
