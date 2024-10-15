f1fc95f77e3ae0744b02391d05f003b55a2c8ff1
Francisco Noriega <frannoriega.92@gmail.com>
2024-10-15 15:16:27 -0300

    menu: Arreglado menu en mobile
    Había unos errores provenientes de la librería hamburger-react.
    La eliminé y dejé un botón hamburguesa estático.
    También actualice el `Drawer` de `vaul` al de `shadcn`.

--- src/components/navbar/actions/menu/index.tsx
+++ src/components/navbar/actions/menu/index.tsx
@@ -4,9 +4,10 @@ import Profile from "@/components/navbar/actions/profile";
 import * as Separator from "@radix-ui/react-separator";
 import Links from "@/components/navbar/actions/links";
 import { useState } from "react";
-import Hamburger from 'hamburger-react';
-import { Drawer } from 'vaul';
 import ThemeButton from "../../theme-button";
+import { Menu as LucideMenu } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 
 type MenuProps = {
   className?: string
@@ -16,20 +17,18 @@ export default function Menu({ className }: MenuProps) {
   const [open, setOpen] = useState(false);
   return (
     <div className={className}>
-      <Drawer.Root direction="right" open={open} onOpenChange={setOpen}>
-        <Drawer.Trigger asChild><Hamburger size={24} toggled={open} toggle={setOpen} /></Drawer.Trigger>
-        <Drawer.Portal>
-          <Drawer.Content className="z-50 max-w-[66.66667%] bg-gray-100 dark:bg-gray-800 flex flex-col rounded-t-[10px] h-full w-[400px] mt-24 fixed bottom-0 right-0">
-            <Profile />
-            <Links listClassName="flex flex-col items-start p-4" />
-            <Separator.Root decorative className="self-center w-2/3 h-px bg-gray-700 dark:bg-gray-400" orientation="horizontal"/>
-            <div className="flex flex-row justify-between items-center p-4">
-              <span>Cambiar tema</span>
-              <ThemeButton />
-            </div>
-          </Drawer.Content>
-        </Drawer.Portal>
-      </Drawer.Root>
+      <Drawer direction="right" open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild><Button variant='ghost'><LucideMenu /></Button></DrawerTrigger>
+        <DrawerContent className="z-50 inset-x-auto max-w-[66.66667%] bg-gray-100 dark:bg-gray-800 flex flex-col rounded-t-[10px] h-full w-[400px] mt-24 fixed bottom-0 right-0">
+          <Profile />
+          <Links listClassName="flex flex-col items-start p-4" />
+          <Separator.Root decorative className="self-center w-2/3 h-px bg-gray-700 dark:bg-gray-400" orientation="horizontal" />
+          <div className="flex flex-row justify-between p-4 pr-8">
+            <span>Cambiar tema</span>
+            <ThemeButton />
+          </div>
+        </DrawerContent>
+      </Drawer>
     </div>
   )
 }
